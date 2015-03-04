3395129a56c982188d774e5a1d5bf2625cba29b3
Sibonay <sibonayk@gmail.com>
2015-03-03 20:15:02 -0500

    fixed onload function init

---
+++ mspacman/README.md
@@ -0,0 +1,4 @@
+Lab 5: Ms Pacman
+
+1) Web page displays the PacMan game board from Google's 30th anniversary special edition, and draws Ms Pac-Man on the game board
+2) Hours spent on project: 3 
--- mspacman/game.js
+++ mspacman/game.js
@@ -1,25 +1,18 @@
-canvas = document.getElementById('game_canvas');
-ctx = canvas.getContext('2d');
+function init() {
+	canvas = document.getElementById('game_canvas');
+	ctx = canvas.getContext('2d');
 
-var image_sprite = new Image();
-image_sprite.src = "pacman10-hp-sprite.png";
-
-image_sprite.addEventListener("load", function() {
-	// draws the blue board
-	ctx.drawImage(image_sprite, 324, 1, 462, 33, 0, 0, 800, 600); 
-	// draws Ms. Pac-Man
-	ctx.drawImage(image_sprite, 80, 18, 20, 20, 30, 25, 20, 20);
-}, false);
-
-
-
+	var image_sprite = new Image();
+	image_sprite.addEventListener("load", function() {
+		// draws the blue board
+		ctx.drawImage(image_sprite, 324, 1, 462, 133, 0, 0, 462, 133); 
+		// draws Ms. Pac-Man
+		ctx.drawImage(image_sprite, 80, 18, 20, 20, 30, 25, 20, 20);
+	}, false);
+	image_sprite.src = "pacman10-hp-sprite.png";
+}
 
 
-//getting background from sprite: 
-//height 1-34
-//width 324 - 786
 
 
-//pacman on background:
-//width starts at 355 or 30
-//height starts at 25
+
