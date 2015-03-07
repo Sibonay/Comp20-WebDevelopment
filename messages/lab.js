aeefbe8b227e23eeb66be1bcf1d935f8a761149d
Sibonay <sibonayk@gmail.com>
2015-03-07 15:21:09 -0500

    something went wrong with files; readded everything

---
+++ messages/lab.js
@@ -0,0 +1,15 @@
+// Your JavaScript goes here...
+
+xhr = new XMLHttpRequest();
+
+xhr.open("get", "data.json", true);
+xhr.onreadystatechange = myCallbackFunction;
+
+xhr.send();
+
+function MyCallbackFunction() {
+	if (xhr.readyState == 4 && xhr.status == 200) {
+		data = JSON.parse(xhr.responseText);
+		document.getElementByID("messages").innerHTML = xhr.responseText;
+	}
+}
