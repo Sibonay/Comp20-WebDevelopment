de0da989b06fd78ed36e224806441a62f398a525
Sibonay <sibonayk@gmail.com>
2015-03-07 15:31:39 -0500

    defined parse function in lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,15 +1,15 @@
-// Your JavaScript goes here...
-
-xhr = new XMLHttpRequest();
-
-xhr.open("get", "data.json", true);
-xhr.onreadystatechange = myCallbackFunction;
-
-xhr.send();
-
-function MyCallbackFunction() {
+function myCallbackFunction() {
 	if (xhr.readyState == 4 && xhr.status == 200) {
 		data = JSON.parse(xhr.responseText);
 		document.getElementByID("messages").innerHTML = xhr.responseText;
 	}
+}
+
+function parse() {
+	xhr = new XMLHttpRequest();
+
+	xhr.open("get", "data.json", true);
+	xhr.onreadystatechange = myCallbackFunction;
+
+	xhr.send();
 }
