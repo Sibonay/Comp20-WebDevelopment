20f4e385bd32f3fde77d6315c2493b82201e544d
Sibonay <sibonayk@gmail.com>
2015-03-08 14:18:37 -0400

    debugging on debugging

--- messages/lab.js
+++ messages/lab.js
@@ -1,6 +1,8 @@
 function myCallbackFunction() {
 	if (xhr.readyState == 4 && xhr.status == 200) {
 		data = JSON.parse(xhr.responseText);
+		console.log(data);
+		console.log(data["content"]);
 		for (i = 0; i < data.length; i++) {   // syntax											// check syntax
 			document.getElementById("messages").innerHTML = 
 			data[i]["content"] + ' ' + data[i]["username"]; 
