9097227fb2ef8ec40307ba4117cfe8283c9c513e
Sibonay <sibonayk@gmail.com>
2015-03-08 14:03:56 -0400

    parsed data into content and username

--- messages/lab.js
+++ messages/lab.js
@@ -1,14 +1,18 @@
 function myCallbackFunction() {
 	if (xhr.readyState == 4 && xhr.status == 200) {
 		data = JSON.parse(xhr.responseText);
-		document.getElementByID("messages").innerHTML = xhr.responseText;
+		for (i = 0; i < data.length; i++) {   // syntax											// check syntax
+			document.getElementById("messages").innerHTML = 
+			data[i][content] + ' ' + data[i][username]; 
+		// how to print elements of a key value pair...
+		}
 	}
 }
 
-function parse() {
+function parse() { // is a diff parse
 	xhr = new XMLHttpRequest();
 
-	xhr.open("get", "data.json", true);
+	xhr.open("GET", "data.json", true);
 	xhr.onreadystatechange = myCallbackFunction;
 
 	xhr.send();
